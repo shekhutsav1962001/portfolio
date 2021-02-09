fc15e34660a2ea1ed62b23493edf12568f7fa468
shekhutsav1962001 <shekhutsav1962001@gmail.com>
2021-02-09 15:31:33 +0530

    header plugin added and ripple added

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+    document.getElementById("hdr").style.opacity = 0.3;
+
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.body.style.backgroundColor = "white";
+    document.getElementById("hdr").style.opacity = 1;
+
+}
+
+
+// water effect
+$(document).ready(function () {
+
+    $('#hdr').ripples({
+        resolution: 512,
+        dropRadius: 10
+    });
+    $('#abc').ripples({
+        resolution: 512,
+        dropRadius: 10
+    });
+});
+
