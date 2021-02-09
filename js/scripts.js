1d6fee74e4f97db84c433ee21f6d0e04d250c989
shekhutsav1962001 <shekhutsav1962001@gmail.com>
2021-02-09 23:00:32 +0530

    scroll bar added and navbar added

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,27 @@
+sideBarOpen = false;
 function openNav() {
+    sideBarOpen = true;
     document.getElementById("mySidenav").style.width = "250px";
-    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
-    document.getElementById("hdr").style.opacity = 0.3;
+
+    // document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+    // document.getElementById("hdr").style.opacity = 0.3;
 
 }
 
 function closeNav() {
+    sideBarOpen = false;
     document.getElementById("mySidenav").style.width = "0";
-    document.body.style.backgroundColor = "white";
-    document.getElementById("hdr").style.opacity = 1;
+    // document.body.style.backgroundColor = "white";
+    // document.getElementById("hdr").style.opacity = 1;
 
 }
 
 
 // water effect
 $(document).ready(function () {
-
     $('#hdr').ripples({
         resolution: 512,
         dropRadius: 10
     });
-    // $('#abc').ripples({
-    //     resolution: 512,
-    //     dropRadius: 10
-    // });
 });
 
